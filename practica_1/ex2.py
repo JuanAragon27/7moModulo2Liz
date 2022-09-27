2afeb3512fdba86aefe3bce7344edd11549887c3
JuanAragon27 <ec2-user@ip-172-31-4-32.us-east-2.compute.internal>
2022-09-27 01:45:14 +0000

    ex1

---
+++ practica_1/ex2.py
@@ -0,0 +1,33 @@
+#Exercise of arrays from 1 to n , you must observe the links between primos
+import sys
+
+m=sys.argv
+z=int(m[1])
+global my_array
+my_array=[]
+global my_new_array
+my_new_array=[]
+
+def my_number(z):
+    for i in range (1,z+1):
+        my_array.append(i)
+        print (my_array)
+def primos(z):
+    global flag
+    flag=0
+    for i in range (1,z):
+        x=int(my_array[i])
+        flag=0
+        for c in range (1,x):
+            if (x/c)!=0 :
+               flag=flag+1
+            if flag <=2:
+                my_new_array.append(i)
+                print(my_new_array)
+            
+            
+            
+
+print(my_new_array)
+my_number(z)
+primos(z)

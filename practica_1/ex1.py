59b24e119dc569d16c41428865ecf918405c4f5c
JuanAragon27 <ec2-user@ip-172-31-4-32.us-east-2.compute.internal>
2022-09-29 21:05:02 +0000

    ex2

--- practica_1/ex1.py
+++ practica_1/ex1.py
@@ -1,2 +1,13 @@
 #First ex "Hello World"
 print("Hello World")
+
+n = int(input("Dame un número: "))
+lista = []
+contador = 0
+for i in range(1, n + 1):
+    if n % i == 0:
+        for j in range (n + 1, n % i == 0 ):
+            contador = contador + 1
+            lista.append(j)
+            print(lista)
+
--- practica_1/ex2.py
+++ practica_1/ex2.py
@@ -6,28 +6,26 @@ z=int(m[1])
 global my_array
 my_array=[]
 global my_new_array
-my_new_array=[]
+my_new_array=[1]
 
 def my_number(z):
     for i in range (1,z+1):
         my_array.append(i)
         
-def primos(z):
-    global flag
-    flag=0
-    for i in range (1,z):
-        x=int(my_array[i])
-        flag=0
-        for c in range (1,x):
-            if (x/c)!=0 :
-               flag=flag+1
-            if flag <=2:
-                my_new_array.append(i)
-                print(my_new_array)
-            
-            
-            
-
-print(my_new_array)
+def Primos(z):
+    cont = 0
+    for i in range(2, z + 1):
+        primos = True
+        for j in range(2,i):
+            if i == j:
+               break
+            elif i%j == 0:
+               primos = False
+            else:
+               continue
+        if primos == True:
+            my_new_array.append(i)
+            cont += 1
+            print(my_new_array)
 my_number(z)
-primos(z)
+Primos(z)
